535c45a11b7b72e3fef98210d613c01c33fdf772
Danil Motyckiy <golbergdon@gmail.com>
2018-02-20 19:21:56 +0200

    add login template

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render("home");
