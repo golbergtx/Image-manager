0491a0e12d1994a144e5d686829b18560f2148bb
Danil Motyckiy <golbergdon@gmail.com>
2018-03-11 18:59:57 +0200

    update(*): add mongo config

---
+++ config/mongo.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dataBaseName = 'imageManager';
+
+const state = {
+    db: null
+};
+
+module.exports.connect = function (callback) {
+    if (state.db) {
+        return callback();
+    }
+    MongoClient.connect(url, function (err, client) {
+        assert.ifError(err);
+        state.db = client.db(dataBaseName);
+        client.close();
+        callback();
+        console.log("connect")
+    });
+};
+
+module.exports.getDataBase = function () {
+    return state.db
+};
+
