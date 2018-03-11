d710b563363543f459074c3b6f83828ee2a3e074
Danil Motyckiy <golbergdon@gmail.com>
2018-03-11 19:27:13 +0200

    update (server): add mongo connection

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const app = express();
 
 const handlebars = require('hbs');
 const bodyParser = require('body-parser');
+const dataBase = require('./config/mongo.js');
 
+//app config
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-
 app.use(express.static(__dirname + '/public'));
+
+
+dataBase.connect(function () {
+    console.log("Connected successfully to server");
+    app.listen(3000, function () {
+        console.log('Api app started');
+    });
+});
 
 app.get('/', function (req, res) {
   res.render("home");
 });
-
-app.listen(3000, function () {
-  console.log('App started');
-}); 
