9266eebbf90c4471bc5d4b1d8c45cc3245ee9d89
Danil Motyckiy <golbergdon@gmail.com>
2018-03-13 00:00:36 +0200

    update (login template): add vue, css, functional (WIP)

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,30 @@
-let loginBtn = document.getElementById("loginBtn");
-let loginSuccessful = false;
-
-function login(body) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'login-check', false);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(body);
-    if (xhr.status != 200) {
-        return false;
-    } else {
-        alert( xhr.responseText );
-        return true;
+let loginForm = new Vue({
+    el: '#loginForm',
+    data: {
+        login: null,
+        password: null,
+        loginSuccessFull: false,
+        showErrorLoginMessage: false
+    },
+    methods: {
+        submit: function () {
+            this.loginSuccessFull = this.loginToServer();
+            if (!this.loginSuccessFull) {
+                this.showErrorLoginMessage = true;
+            }
+        },
+        loginToServer: function (body) {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'login-check', false);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(body);
+            if (xhr.status != 200) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
-}
-
-loginBtn.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    loginSuccessful = login("test");
-
-    console.log(loginSuccessful)
 });
+//debug
+console.log(loginForm);
